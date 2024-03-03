bf53a6776992f8a15c3228639a0a9f5eba513a72
markj6gar <mark.j6gar@gmail.com>
2024-03-03 21:14:38 +0000

    constructors, accessors and setEmail

--- src/ca2/gnomesltd/Employee.java
+++ src/ca2/gnomesltd/Employee.java
@@ -14,6 +14,43 @@ public class Employee {
     private String name;
     private String email;
     private int empNum;
+    private static int nextEmpNum = 1; //nextEmpNum, of type int and initialized to 1, 
+    
+    //constructuor with defult values 
+   public Employee() {
+       this.name = "";
+       this.email = "";
+       this.empNum = nextEmpNum; // which initialises empNum in the constructor. 
+       nextEmpNum++; // incremented to give consecutive numbers to each newly created object
+   }
+   
+   // constructor with parameters
+    public Employee(String name, String email) {
+       this.name = name;
+       this.email = email;
+       this.empNum = nextEmpNum;
+       nextEmpNum++;
+    }
+    //Accessor methods to return name, email and empNum values.
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public int getEmpNum() {
+        return empNum;
+    }
+    
+    public void setEmail(String email) {
+      if (email.length() > 3) { 
+          this.email = email;
+      } else {
+          System.out.println("Email must be more than 3 characthers.");
+      }
+      }
+    }
+    
 }
     
 
